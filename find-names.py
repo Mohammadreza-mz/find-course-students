c6d330f75511cf2da1559e2442258f605f81f11e
Mohammadreza <mohammadrezamhmz@gmail.com>
2022-10-01 21:21:36 +0330

    read student ids from stdin

--- find-names.py
+++ find-names.py
@@ -1,11 +1,6 @@
 import pandas as pd
 
 df = pd.read_csv('students-ids.csv')
-ids = []
-with open('results.txt') as f:
-    lines = f.readlines()
-    for line in lines:
-        ids.append(int(line[:-1]))
+ids = list(map(int, input().split(" ")))
 
-res = df.loc[df['id'].isin(ids)]
-print(res)
+print(df.loc[df['id'].isin(ids)])
